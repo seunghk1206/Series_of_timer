a0bf9b929d31d87f101314ffc4b84775fc389785
Noble-1206 <seunghk1206@gmail.com>
2021-05-05 02:16:10 +0900

    Update test.py

--- SAT_timer/test.py
+++ SAT_timer/test.py
@@ -1,10 +1,46 @@
 import time
 import sys
-print(2%2)
-for remaining in range(10, 0, -1):
-    sys.stdout.write("\r")
-    sys.stdout.write("{:2d} seconds remaining.".format(remaining)) 
-    sys.stdout.flush()
-    time.sleep(1)
+def timer(NoS):
+    start1 = time.time()
+    totalTime = 0
+    for remaining in range(NoS, 0, -1):
+        start = time.time()
+        a = remaining-(3600*(remaining//3600))
+        b = a//60
+        c = remaining-(remaining//3600*3600)-(b*60)
+        sys.stdout.write("\r")
+        sys.stdout.write("{:2d} seconds remaining.".format(remaining)) 
+        sys.stdout.flush()
+        print(remaining//3600, ':', b, ':', c)
+        time.sleep(1-(time.time()-start)-0.0083)
+        totalTime += time.time()-start
+        print(time.time()-start)
+    print("average: " + str(totalTime/NoS))
+    print(time.time()-start1)
+    time.sleep(5)
 
-sys.stdout.write("\rComplete!            \n")
+timer(7200)
+'''
+import time
+import sys
+def timer(NoS):
+    start1 = time.time()
+    totalTime = 0
+    for remaining in range(NoS, 0, -1):
+        start = time.time()
+        a = remaining-(3600*(remaining//3600))
+        b = a//60
+        c = remaining-(remaining//3600*3600)-(b*60)
+        sys.stdout.write("\r")
+        sys.stdout.write("{:2d} seconds remaining.".format(remaining)) 
+        sys.stdout.flush()
+        print(remaining//3600, ':', b, ':', c)
+        time.sleep(1-(time.time()-start)-0.0083)
+        totalTime += time.time()-start
+        print(time.time()-start)
+    print("average: " + str(totalTime/NoS))
+    print(time.time()-start1)
+    time.sleep(5)
+
+timer(60)
+'''
